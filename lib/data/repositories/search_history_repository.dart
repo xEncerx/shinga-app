135b2faa2b9ec0653c4488ecfbe9febde16b4d94
encer <encer.e@yandex.ru>
2025-06-20 18:35:00 +0300

    refactor(search): improve search history management and add documentation

--- lib/data/repositories/search_history_repository.dart
+++ lib/data/repositories/search_history_repository.dart
@@ -5,6 +5,7 @@ class SearchHistoryRepository {
 
   final HiveDatasource _hiveDatasource;
 
+  /// Returns a list of search queries sorted by timestamp in descending order
   List<String?> getSearchHistory() {
     final data = _hiveDatasource.searchHistoryBox.values.toList();
 
@@ -13,30 +14,37 @@ class SearchHistoryRepository {
     return data.map((item) => item.query).toList();
   }
 
+  /// Adds a new search query to the search history
+  /// If the query already exists, it updates the timestamp
   Future<void> addToSearchHistory({required String value}) async {
     if (value.trim().isEmpty) return;
 
-    // TODO: History should be limited to 50 items
-
-    final data = _hiveDatasource.searchHistoryBox.values;
+    final historyValues = _hiveDatasource.searchHistoryBox.values;
 
     SearchHistoryItem? item;
     try {
-      item = data.firstWhere((item) => item.query == value);
-    } catch (_) {
-      item = null;
-    }
+      item = historyValues.firstWhere((item) => item.query == value);
+    } catch (_) {}
 
     if (item != null) {
       item.timestamp = DateTime.now();
       await item.save();
+      // Create a new item if it doesn't exist
     } else {
       await _hiveDatasource.searchHistoryBox.add(
         SearchHistoryItem(query: value),
       );
+      // Remove the oldest item if the history exceeds 50 items
+      if (historyValues.length > 50) {
+        final oldestItem = historyValues.reduce(
+          (a, b) => a.timestamp.isBefore(b.timestamp) ? a : b,
+        );
+        await oldestItem.delete();
+      }
     }
   }
 
+  /// Clears the search history
   Future<void> clearSearchHistory() async {
     await _hiveDatasource.searchHistoryBox.clear();
   }
